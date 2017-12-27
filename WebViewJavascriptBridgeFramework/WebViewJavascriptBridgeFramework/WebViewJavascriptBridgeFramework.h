f7a00b72184f87501816456449bd0b465baf40d0
Jose Miguel Benedicto Ruiz <jmbene@gmail.com>
2017-12-27 18:40:44 +0100

    Fixing typo
    Fixing typo

---
+++ WebViewJavascriptBridgeFramework/WebViewJavascriptBridgeFramework/WebViewJavascriptBridgeFramework.h
@@ -0,0 +1,22 @@
+//
+//  WebViewJavascriptBridgeFramework.h
+//  WebViewJavascriptBridgeFramework
+//
+//  Created by Jose Miguel Benedicto Ruiz on 27/12/2017.
+//  Copyright © 2017 FinanceApp AG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WebViewJavascriptBridgeFramework.
+FOUNDATION_EXPORT double WebViewJavascriptBridgeFrameworkVersionNumber;
+
+//! Project version string for WebViewJavascriptBridgeFramework.
+FOUNDATION_EXPORT const unsigned char WebViewJavascriptBridgeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebViewJavascriptBridgeFramework/PublicHeader.h>
+
+#include <WebViewJavascriptBridge/WebViewJavascriptBridge_JS.h>
+#include <WebViewJavascriptBridge/WebViewJavascriptBridgeBase.h>
+#include <WebViewJavascriptBridge/WebViewJavascriptBridge.h>
+#include <WebViewJavascriptBridge/WKWebViewJavascriptBridge.h>
